7d4220391ab7b6b421e1fb70cf84278e7b82fa5b
Beemil <valeskavr17@gmail.com>
2025-08-14 21:32:55 -0600

    Mostrar informacion en Contador
    También corregir para que al herencia se efectue

--- StartUp/Contador.cs
+++ StartUp/Contador.cs
@@ -2,10 +2,18 @@
 
 namespace StartUp
 {
-    public class Contador
+    public class Contador : Empleado
     {
         public int HorasExtras { get; set; }
-        public double PagoPorHoraExtra { get; set; }    
+        public double PagoPorHoraExtra { get; set; }  
+
+        public Contador() { }
+        public Contador(string nombre, int id, double salarioBase, int horasExtras, double pagoPorHoraExtra)
+            : base(nombre, id, salarioBase)
+        {
+            HorasExtras = horasExtras;
+            PagoPorHoraExtra = pagoPorHoraExtra;
+        }
 
         public override double CalcularSalarioAnual()
         {
@@ -17,7 +25,9 @@ namespace StartUp
             Console.WriteLine($"ID: {ID}");
             Console.WriteLine($"Horas Extras: {HorasExtras}");
             Console.WriteLine($"Pago por Hora Extra: {PagoPorHoraExtra}");
+            Console.WriteLine($"Salario Base: {SalarioBase}");
             Console.WriteLine($"Salario Anual: {CalcularSalarioAnual()}");
+            Console.WriteLine();
         }
     }
 }
