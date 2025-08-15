e94d88319ecdd6a06d3a172d6821933bf7f9a012
Beemil <valeskavr17@gmail.com>
2025-08-14 21:32:25 -0600

    Mostrar informacion en desarrollador

--- StartUp/Desarrollador.cs
+++ StartUp/Desarrollador.cs
@@ -2,9 +2,16 @@
 
 namespace StartUp
 {
-    public class Desarrollador
+    public class Desarrollador : Empleado
     {
         public string LenguajePrincipal { get; set; }
+
+        public Desarrollador() { }  
+        public Desarrollador(string nombre, int id, double salarioBase, string lenguajePrincipal)
+            : base(nombre, id, salarioBase)
+        {
+            LenguajePrincipal = lenguajePrincipal;
+        }
 
         public override double CalcularSalarioAnual()
         {
@@ -15,7 +22,9 @@ namespace StartUp
             Console.WriteLine($"Desarrollador: {Nombre}");
             Console.WriteLine($"ID: {ID}");
             Console.WriteLine($"Lenguaje Principal: {LenguajePrincipal}");
+            Console.WriteLine($"Salario Base: {SalarioBase}");
             Console.WriteLine($"Salario Anual: {CalcularSalarioAnual()}");
+            Console.WriteLine();
         }
     }
 }
