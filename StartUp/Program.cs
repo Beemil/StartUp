c059a5b3dc2b4e7081b340a16ef096034767e84d
Beemil <valeskavr17@gmail.com>
2025-08-14 21:33:13 -0600

    Colocar Datos para Consola

--- StartUp/Program.cs
+++ StartUp/Program.cs
@@ -1,15 +1,32 @@
 ﻿using StartUp;
 
-class Program
-{
-    Gerente gerente = new Gerente();
-    gerente.Nombre = "Cristian Almendarez";
-    gerente.ID = 1;
-    gerente.SalarioBase = 22000;
-    gentente.BonoAnual = 5000;
-   
+Gerente gerente = new Gerente();
+gerente.Nombre = "Cristian Almendarez";
+gerente.ID = 1;
+gerente.SalarioBase = 22000;
+gerente.BonoAnual = 5000;
+gerente.MostrarInformacion();
 
 
-}
+Desarrollador desarrollador = new Desarrollador();
+desarrollador.Nombre = "Valeska Ventura";
+desarrollador.ID = 2;
+desarrollador.SalarioBase = 18000;
+desarrollador.LenguajePrincipal = "C#";
+desarrollador.MostrarInformacion();
+
+Contador contador = new Contador();
+contador.Nombre = "Mercedes Urbina";
+contador.ID = 3;
+contador.SalarioBase = 25000;
+contador.HorasExtras = 10;
+contador.PagoPorHoraExtra = 25;
+contador.MostrarInformacion();
+
+
+
+
+
+
 
 
